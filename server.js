94922601ba2dee28bd0ac1aefc84553ccbe14083
ironreplica <trevorchilds8@gmail.com>
2023-10-21 11:00:41 -0600

    Changes for render web service

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 app.use("/ttech", require("./routes"));
+
 app.listen(port,()=>{
     console.log(`Test server running on port: ${port}`);
 });
